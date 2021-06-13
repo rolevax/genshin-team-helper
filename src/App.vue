c5fbef01702bd605e186af46a6975eacdaf93b75
rolevax <rolevax@outlook.com>
2021-06-14 01:29:44 +0800

    change page title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div :style="{ width: pageWidth, margin: '0 auto'}">
-    <h1>原神大地图配队助手</h1>
+    <h1>原神大地图配队助手（开发中）</h1>
     <el-divider>喵打出品，都是垃圾</el-divider>
     <h2>输入你需要的功能，结合 BOX 与 XP，自动计算配队方案</h2>
     <MainForm />
@@ -29,6 +29,9 @@ export default {
     isMobile() {
       return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     }
+  },
+  metaInfo: {
+    title: "原神大地图配队助手 | 喵打"
   }
 }
 </script>
