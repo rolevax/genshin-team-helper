c39cb0a044d2038a07d4f8fab79008e1e3c6b996
rolevax <rolevax@outlook.com>
2021-06-27 05:05:06 +0800

    update header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div :style="{ width: pageWidth, margin: '0 auto'}">
-    <h1>原神大地图配队助手（开发中）</h1>
-    更新于 2021-06-26
+    <h1>原神大地图配队助手</h1>
+    更新于 2021-06-26，同步至 1.6 可莉池
     <el-divider></el-divider>
     <h3>输入你需要的功能，结合 BOX 与 XP，自动计算配队方案</h3>
     <MainForm />
